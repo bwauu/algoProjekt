290c3162d1cb44dcdd2fe7611cd346fe3a498579
bwa1337 <bwa1337@bitbucket.org>
2021-03-15 15:47:49 +0100

    clean slate

--- src/algo/weatherdata/WeatherDataHandler.java
+++ src/algo/weatherdata/WeatherDataHandler.java
@@ -3,7 +3,6 @@ package algo.weatherdata;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.*;
@@ -12,95 +11,105 @@ import java.util.*;
  * Retrieves temperature data from a weather station file.
  */
 public class WeatherDataHandler {
-	Demo demo = new Demo();
-	/**
-	 * Load weather data from file.
-	 *
-	 * @param filePath path to file with weather data
-	 * @throws IOException if there is a problem while reading the file
-	 * @return
-	 */
-	public void loadData(String filePath) throws IOException{
-		String myPath = "C:\\Users\\wiapp\\IdeaProjects\\algoProjekt\\src\\" + filePath;
-		List<String> fileData = Files.readAllLines(Paths.get(myPath));
-		Map<Integer,Demo> map=new HashMap<Integer, Demo>();
+    Demo demo = new Demo();
 
-		//TODO: Structure data and put it in appropriate data structure
+    /**
+     * Load weather data from file.
+     *
+     * @param filePath path to file with weather data
+     * @return
+     * @throws IOException if there is a problem while reading the file
+     */
+    public void loadData(String filePath) throws IOException {
+        String myPath = "C:\\Users\\wiapp\\IdeaProjects\\algoProjekt\\src\\" + filePath;
+        List<String> fileData = Files.readAllLines(Paths.get(myPath));
+		Map dataHodler = new HashMap<Integer, LinkedList>();
+				//TODO: Structure data and put it in appropriate data structure
+        Scanner scanner = new Scanner(new File(myPath));
+
 		int index = 0;
-		Scanner scanner = new Scanner(new File(myPath));
+		while (scanner.hasNext()) {
 
-		while (scanner.hasNext()){
-			String line = scanner.nextLine();
-			String[] array = line.split("\\;",4);
-			Scanner lineScanner = new Scanner(line);
-			lineScanner.useDelimiter(";");
+			for (String fileDatum : fileData) {
+				String line = scanner.nextLine();
+				String[] array = line.split("\\;", 4);
+				Scanner lineScanner = new Scanner(line);
+				lineScanner.useDelimiter(";");
+				String date = array[0];
 
-			//Set<Integer> keySet= map.keySet();
+				//Set<Integer> keySet= map.keySet();
+				dataHodler.put(index,fileDatum);
+				index++;
+				/*
+				String date = array[0];
 
-			String date = array[0];
+				String time = array[1];
+				String degrees = array[2];
+				String approval = array[3];
 
-			String time = array[1];
-			String degrees = array[2];
-			String approval = array[3];
+				 */
 
-			for (Map.Entry<Integer, Demo> entry : map.entrySet()) {
-				System.out.println(entry.getKey() + "/" + entry.getValue());
 			}
+
 
 
 
 		}
 
+		System.out.println(dataHodler.get(0));
 		scanner.close();
 	}
-	/**
-	 * Search for average temperature for all dates between the two dates (inclusive).
-	 * Result is sorted by date (ascending). When searching from 2000-01-01 to 2000-01-03
-	 * the result should be:
-	 * 2000-01-01 average temperature: 0.42 degrees Celsius
-	 * 2000-01-02 average temperature: 2.26 degrees Celsius
-	 * 2000-01-03 average temperature: 2.78 degrees Celsius
-	 *
-	 * @param dateFrom start date (YYYY-MM-DD) inclusive
-	 * @param dateTo end date (YYYY-MM-DD) inclusive
-	 * @return average temperature for each date, sorted by date
-	 */
-	public List<String> averageTemperatures(LocalDate dateFrom, LocalDate dateTo) {
-		//TODO: Implements method
 
 
-		return null;
-	}
+    /**
+     * Search for average temperature for all dates between the two dates (inclusive).
+     * Result is sorted by date (ascending). When searching from 2000-01-01 to 2000-01-03
+     * the result should be:
+     * 2000-01-01 average temperature: 0.42 degrees Celsius
+     * 2000-01-02 average temperature: 2.26 degrees Celsius
+     * 2000-01-03 average temperature: 2.78 degrees Celsius
+     *
+     * @param dateFrom start date (YYYY-MM-DD) inclusive
+     * @param dateTo   end date (YYYY-MM-DD) inclusive
+     * @return average temperature for each date, sorted by date
+     */
+    public List<String> averageTemperatures(LocalDate dateFrom, LocalDate dateTo) {
+        //TODO: Implements method
 
 
-	/**
-	 * Search for missing values between the two dates (inclusive) assuming there
-	 * should be 24 measurement values for each day (once every hour). Result is
-	 * sorted by number of missing values (descending). When searching from
-	 * 2000-01-01 to 2000-01-03 the result should be:
-	 * 2000-01-02 missing 1 values
-	 * 2000-01-03 missing 1 values
-	 * 2000-01-01 missing 0 values
-	 *
-	 * @param dateFrom start date (YYYY-MM-DD) inclusive
-	 * @param dateTo end date (YYYY-MM-DD) inclusive
-	 * @return dates with missing values together with number of missing values for each date, sorted by number of missing values (descending)
-	 */
-	public List<String> missingValues(LocalDate dateFrom, LocalDate dateTo) {
-		//TODO: Implements method
-		return null;
-	}
-	/**
-	 * Search for percentage of approved values between the two dates (inclusive).
-	 * When searching from 2000-01-01 to 2000-01-03 the result should be:
-	 * Approved values between 2000-01-01 and 2000-01-03: 32.86 %
-	 *
-	 * @param dateFrom start date (YYYY-MM-DD) inclusive
-	 * @param dateTo end date (YYYY-MM-DD) inclusive
-	 * @return period and percentage of approved values for the period
-	 */
-	public List<String> approvedValues(LocalDate dateFrom, LocalDate dateTo) {
-		//TODO: Implements method
-		return null;
-	}
+        return null;
+    }
+
+
+    /**
+     * Search for missing values between the two dates (inclusive) assuming there
+     * should be 24 measurement values for each day (once every hour). Result is
+     * sorted by number of missing values (descending). When searching from
+     * 2000-01-01 to 2000-01-03 the result should be:
+     * 2000-01-02 missing 1 values
+     * 2000-01-03 missing 1 values
+     * 2000-01-01 missing 0 values
+     *
+     * @param dateFrom start date (YYYY-MM-DD) inclusive
+     * @param dateTo   end date (YYYY-MM-DD) inclusive
+     * @return dates with missing values together with number of missing values for each date, sorted by number of missing values (descending)
+     */
+    public List<String> missingValues(LocalDate dateFrom, LocalDate dateTo) {
+        //TODO: Implements method
+        return null;
+    }
+
+    /**
+     * Search for percentage of approved values between the two dates (inclusive).
+     * When searching from 2000-01-01 to 2000-01-03 the result should be:
+     * Approved values between 2000-01-01 and 2000-01-03: 32.86 %
+     *
+     * @param dateFrom start date (YYYY-MM-DD) inclusive
+     * @param dateTo   end date (YYYY-MM-DD) inclusive
+     * @return period and percentage of approved values for the period
+     */
+    public List<String> approvedValues(LocalDate dateFrom, LocalDate dateTo) {
+        //TODO: Implements method
+        return null;
+    }
 }
