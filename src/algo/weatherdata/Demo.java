50fa4089ad36146cb8eadf0a42d49f720a29c90b
bwa1337 <bwa1337@bitbucket.org>
2021-03-17 15:12:51 +0100

    Working version sout every year in loop:

--- src/algo/weatherdata/Demo.java
+++ src/algo/weatherdata/Demo.java
@@ -1,21 +1,70 @@
 package algo.weatherdata;
 
+import java.awt.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 class Demo {
-    Map<Integer,Demo> map=new HashMap<Integer, Demo>();
+    HashMap demoHashMap;
 
+    {
+        demoHashMap = new HashMap<Integer, LinkedList>();
+    }
+
+    public Demo(int index, String date, String time, String degrees, String approval) {
+    }
+
+
+    public int getKey() {
+        return this.key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getApproval() {
+        return approval;
+    }
+
+    public void setApproval(String approval) {
+        this.approval = approval;
+    }
 
     private int key;
 
-    private LocalDate date;
-    private LocalTime time;
-    private int temperature;
-    private Boolean approval;
+    private String date;
+    private String time;
+    private String temperature;
+    private String approval;
 
 
 
@@ -23,45 +72,7 @@ class Demo {
 
     }
 
-    public int getKey() {
-        return key;
-    }
 
-    public void setKey(int key) {
-        this.key = key;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
-
-    public void setTime(LocalTime time) {
-        this.time = time;
-    }
-
-    public int getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(int temperature) {
-        this.temperature = temperature;
-    }
-
-    public Boolean getApproval() {
-        return approval;
-    }
-
-    public void setApproval(Boolean approval) {
-        this.approval = approval;
-    }
     /*
     @Override
     public String toString() {
